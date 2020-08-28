843a5ef5f9229ea25af54a90db146e490662ae2e
camilagerarde <millagerarde@gmail.com>
2020-08-28 14:06:38 -0300

    dotenv

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -1,26 +1,31 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    username: "postgres",
-    password: "example",
-    database: "api_bq_development",
-    host: "db",
-    dialect: "postgres",
+    username: 'postgres',
+    password: 'example',
+    database: 'api_bq_development',
+    host: 'db',
+    dialect: 'postgres',
     operatorsAliases: false,
   },
   test: {
-    username: "postgres",
-    password: "example",
-    database: "api_bq_test",
-    host: "db",
-    dialect: "postgres",
+    username: 'postgres',
+    password: 'example',
+    database: 'api_bq_test',
+    host: 'db',
+    dialect: 'postgres',
     operatorsAliases: false,
   },
   production: {
-    username: "postgres",
-    password: "example",
-    database: "api_bq",
-    host: "db",
-    dialect: "postgres",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
     operatorsAliases: false,
+    dialectOptions: {
+      ssl: true,
+    },
   },
 };
